e71cf738964ecd26539694478146243ddfb1c284
Sarah Darnell <sarahd1707@gmail.com>
2025-05-15 14:41:05 -0500

    table 2a draft

--- UCA Part 4.R
+++ UCA Part 4.R
@@ -56,3 +56,52 @@ ft <- flextable(table2b) %>%
 read_docx() %>%
   body_add_flextable(ft) %>%
   print(target = "table2b.docx")
+
+##Table 2a - median results and kruskal wallis results for dys and hc
+table2a <- uca %>%
+  select(all_of(contvar), t1group) %>%
+  filter(!t1group %in% c("Fibroid", "Endometriosis")) %>%
+  group_by(t1group) %>%
+  summarize(across(everything(), ~ sprintf("%.1f [%.1f-%.1f]", 
+                                           median(., na.rm = TRUE), 
+                                           quantile(., 0.25, na.rm = TRUE),
+                                           quantile(., 0.75, na.rm = TRUE))),
+            .groups = "drop") %>%
+  pivot_longer(cols = -t1group, names_to = "Item", values_to = "Median [IQR]") %>% 
+  pivot_wider(names_from = t1group, values_from = `Median [IQR]`) 
+
+#kruskal wallis test
+uca_filtered_groups <- uca %>%
+  filter(t1group %in% c("Dysmenorrhea", "Pain Free Control"))
+
+kw <- lapply(contvar, function(var) {
+  formula <- as.formula(paste(var, "~t1group"))
+  test_result <- kruskal.test(formula, data = uca_filtered_groups)
+  
+  data.frame(
+    Variable = var, 
+    Chi_Square = test_result$statistic, 
+    df = test_result$parameter, 
+    p_value = test_result$p.value, 
+    stringsAsFactors = FALSE
+  )
+})
+
+kw_results <- do.call(rbind, kw)
+
+#combine into one table
+names(table2a)[1] <- "Variable"
+
+table2a_full <- left_join(table2a, kw_results, by = "Variable")
+
+# Create a flextable object
+ft2 <- flextable(table2a_full) %>%
+  bold(i = 1, part = "header") %>%               # Bold the header row
+  align(align = "left", part = "all") %>%         # Align left for all parts
+  fontsize(size = 10, part = "all") %>%           # Set font size
+  set_table_properties(layout = "fixed", width = 1) %>% # Fixed width layout
+  theme_vanilla()                                # Apply a vanilla theme
+
+read_docx() %>%
+  body_add_flextable(ft2) %>%
+  print(target = "table2a.docx")
