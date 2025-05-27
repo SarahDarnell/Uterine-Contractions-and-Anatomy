16a9d3441e32890369e018f03aa6c0b5ccb2e133
Sarah Darnell <sarahd1707@gmail.com>
2025-05-27 15:48:05 -0500

    Making subset tables of 2a to determine what should be included in final

--- UCA Part 4.R
+++ UCA Part 4.R
@@ -1,5 +1,5 @@
 #Uterine Contractions and Anatomy Analysis - Part 4 - Full dataset - table 2s
-#Written by Sarah Darnell, began 5.15.25, lasted edited 5.15.25
+#Written by Sarah Darnell, began 5.15.25, lasted edited 5.27.25
 
 library(readr)
 library(tableone)
@@ -88,8 +88,227 @@ ggplot(uca_pain, aes(x = t1group, y = Value, fill = t1group)) +
        y = "Value") +
   theme_minimal()
 
+##KH requests knew smaller tables, as described below
 
-##below here needs to be work shopped##
+#table 2c - nonmenses data (eh16 only) for contvars
+#Remove menses visits from eh16
+uca_filtered_nonmenses <- uca %>%
+  filter(menses == 0)
+
+table2c <- uca_filtered_nonmenses %>%
+  select(all_of(contvar), t1group) %>%
+  filter(!t1group %in% c("Fibroid", "Endometriosis")) %>%
+  group_by(t1group) %>%
+  summarize(across(everything(), ~ sprintf("%.1f [%.1f-%.1f]", 
+                                           median(., na.rm = TRUE), 
+                                           quantile(., 0.25, na.rm = TRUE),
+                                           quantile(., 0.75, na.rm = TRUE))),
+            .groups = "drop") %>%
+  pivot_longer(cols = -t1group, names_to = "Item", values_to = "Median [IQR]") %>% 
+  pivot_wider(names_from = t1group, values_from = `Median [IQR]`) 
+
+#kruskal wallis test
+uca_filtered_groups <- uca_filtered_nonmenses %>%
+  filter(t1group %in% c("Dysmenorrhea", "Pain Free Control"))
+
+kw <- lapply(contvar, function(var) {
+  formula <- as.formula(paste(var, "~t1group"))
+  # Check if variable has enough data to run test
+  temp <- uca_filtered_groups[, c(var, "t1group")]
+  temp <- temp[complete.cases(temp), ]  # remove NAs
+  
+  if (nlevels(temp$t1group) < 2 || length(unique(temp[[var]])) < 2) {
+    return(NULL)  # skip this variable
+  }
+  test_result <- kruskal.test(formula, data = uca_filtered_groups)
+  
+  data.frame(
+    Variable = var, 
+    Chi_Square = test_result$statistic, 
+    df = test_result$parameter, 
+    p_value = test_result$p.value, 
+    stringsAsFactors = FALSE
+  )
+})
+
+kw_results <- do.call(rbind, kw)
+
+#combine into one table
+names(table2c)[1] <- "Variable"
+
+table2c_full <- left_join(table2c, kw_results, by = "Variable")
+
+# Create a flextable object
+ft2 <- flextable(table2c_full) %>%
+  bold(i = 1, part = "header") %>%               # Bold the header row
+  align(align = "left", part = "all") %>%         # Align left for all parts
+  fontsize(size = 10, part = "all") %>%           # Set font size
+  set_table_properties(layout = "fixed", width = 1) %>% # Fixed width layout
+  theme_vanilla()                                # Apply a vanilla theme
+
+read_docx() %>%
+  body_add_flextable(ft2) %>%
+  print(target = "table2c.docx")
+
+#table 2d - only menses data for contvars
+#Remove nonmenses visits from eh16
+uca_filtered_menses <- uca %>%
+  filter(menses == 1 | is.na(menses))
+
+table2d <- uca_filtered_menses %>%
+  select(all_of(contvar), t1group) %>%
+  filter(!t1group %in% c("Fibroid", "Endometriosis")) %>%
+  group_by(t1group) %>%
+  summarize(across(everything(), ~ sprintf("%.1f [%.1f-%.1f]", 
+                                           median(., na.rm = TRUE), 
+                                           quantile(., 0.25, na.rm = TRUE),
+                                           quantile(., 0.75, na.rm = TRUE))),
+            .groups = "drop") %>%
+  pivot_longer(cols = -t1group, names_to = "Item", values_to = "Median [IQR]") %>% 
+  pivot_wider(names_from = t1group, values_from = `Median [IQR]`) 
+
+#kruskal wallis test
+uca_filtered_groups <- uca_filtered_menses %>%
+  filter(t1group %in% c("Dysmenorrhea", "Pain Free Control"))
+
+kw <- lapply(contvar, function(var) {
+  formula <- as.formula(paste(var, "~t1group"))
+  # Check if variable has enough data to run test
+  temp <- uca_filtered_groups[, c(var, "t1group")]
+  temp <- temp[complete.cases(temp), ]  # remove NAs
+  
+  if (nlevels(temp$t1group) < 2 || length(unique(temp[[var]])) < 2) {
+    return(NULL)  # skip this variable
+  }
+  test_result <- kruskal.test(formula, data = uca_filtered_groups)
+  
+  data.frame(
+    Variable = var, 
+    Chi_Square = test_result$statistic, 
+    df = test_result$parameter, 
+    p_value = test_result$p.value, 
+    stringsAsFactors = FALSE
+  )
+})
+
+kw_results <- do.call(rbind, kw)
+
+#combine into one table
+names(table2d)[1] <- "Variable"
+
+table2d_full <- left_join(table2d, kw_results, by = "Variable")
+
+# Create a flextable object
+ft2 <- flextable(table2d_full) %>%
+  bold(i = 1, part = "header") %>%               # Bold the header row
+  align(align = "left", part = "all") %>%         # Align left for all parts
+  fontsize(size = 10, part = "all") %>%           # Set font size
+  set_table_properties(layout = "fixed", width = 1) %>% # Fixed width layout
+  theme_vanilla()                                # Apply a vanilla theme
+
+read_docx() %>%
+  body_add_flextable(ft2) %>%
+  print(target = "table2d.docx")
+
+#table 2e - only menses data for contvars, with HC not able to have 
+#pain over a 1, and dys having to have pain over a 2
+
+#Remove nonmenses visits from eh16
+uca_filtered_menses <- uca %>%
+  filter(menses == 1 | is.na(menses)) 
+
+#Remove HC w/pain and DYS w/o pain
+uca_filtered_menses_pain <- uca_filtered_menses %>%
+  filter(
+      #EH16, DYS, pain > 2
+      (t1group == "Dysmenorrhea" & study == "EH16" &
+       (is.na(`Menses Visit: Maximum cramping pain post scan`) |
+          `Menses Visit: Maximum cramping pain post scan` > 2)) |
+      #EH19, DYS, pain > 2
+      (t1group == "Dysmenorrhea" & study == "EH19" &
+         (is.na(`Visit 1: Maximum cramping pain scan 1`) | 
+            `Visit 1: Maximum cramping pain scan 1` > 2) & 
+         (is.na(`Visit 1: Maximum cramping pain scan 2`) | 
+            `Visit 1: Maximum cramping pain scan 2` > 2) &
+         (is.na(`Visit 2: Maximum cramping pain scan 1`) | 
+            `Visit 2: Maximum cramping pain scan 1` > 2) &
+         (is.na(`Visit 2: Maximum cramping pain scan 2`) | 
+            `Visit 2: Maximum cramping pain scan 2` > 2)) |
+      #EH16, HC, pain = 0
+      (t1group == "Pain Free Control" & study == "EH16" &
+         (is.na(`Menses Visit: Maximum cramping pain post scan`) |
+            `Menses Visit: Maximum cramping pain post scan` == 0)) |
+      #EH19, HC, pain = 0
+      (t1group == "Pain Free Control" & study == "EH19" &
+         (is.na(`Visit 1: Maximum cramping pain scan 1`) | 
+            `Visit 1: Maximum cramping pain scan 1` == 0) & 
+         (is.na(`Visit 1: Maximum cramping pain scan 2`) | 
+            `Visit 1: Maximum cramping pain scan 2` == 0) &
+         (is.na(`Visit 2: Maximum cramping pain scan 1`) | 
+            `Visit 2: Maximum cramping pain scan 1` == 0) &
+         (is.na(`Visit 2: Maximum cramping pain scan 2`) | 
+            `Visit 2: Maximum cramping pain scan 2` == 0))
+  )
+    
+table2e <- uca_filtered_menses_pain %>%
+  select(all_of(contvar), t1group) %>%
+  filter(!t1group %in% c("Fibroid", "Endometriosis")) %>%
+  group_by(t1group) %>%
+  summarize(across(everything(), ~ sprintf("%.1f [%.1f-%.1f]", 
+                                           median(., na.rm = TRUE), 
+                                           quantile(., 0.25, na.rm = TRUE),
+                                           quantile(., 0.75, na.rm = TRUE))),
+            .groups = "drop") %>%
+  pivot_longer(cols = -t1group, names_to = "Item", values_to = "Median [IQR]") %>% 
+  pivot_wider(names_from = t1group, values_from = `Median [IQR]`) 
+
+#kruskal wallis test
+uca_filtered_groups <- uca_filtered_menses_pain %>%
+  filter(t1group %in% c("Dysmenorrhea", "Pain Free Control"))
+
+kw <- lapply(contvar, function(var) {
+  formula <- as.formula(paste(var, "~t1group"))
+  # Check if variable has enough data to run test
+  temp <- uca_filtered_groups[, c(var, "t1group")]
+  temp <- temp[complete.cases(temp), ]  # remove NAs
+  
+  if (nlevels(temp$t1group) < 2 || length(unique(temp[[var]])) < 2) {
+    return(NULL)  # skip this variable
+  }
+  test_result <- kruskal.test(formula, data = uca_filtered_groups)
+  
+  data.frame(
+    Variable = var, 
+    Chi_Square = test_result$statistic, 
+    df = test_result$parameter, 
+    p_value = test_result$p.value, 
+    stringsAsFactors = FALSE
+  )
+})
+
+kw_results <- do.call(rbind, kw)
+
+#combine into one table
+names(table2e)[1] <- "Variable"
+
+table2e_full <- left_join(table2e, kw_results, by = "Variable")
+
+# Create a flextable object
+ft2 <- flextable(table2e_full) %>%
+  bold(i = 1, part = "header") %>%               # Bold the header row
+  align(align = "left", part = "all") %>%         # Align left for all parts
+  fontsize(size = 10, part = "all") %>%           # Set font size
+  set_table_properties(layout = "fixed", width = 1) %>% # Fixed width layout
+  theme_vanilla()                                # Apply a vanilla theme
+
+read_docx() %>%
+  body_add_flextable(ft2) %>%
+  print(target = "table2e.docx")
+
+
+
+
+##below here needs to be work shopped for final table 2a##
 
 table2a <- uca %>%
   select(all_of(contvar), t1group) %>%
